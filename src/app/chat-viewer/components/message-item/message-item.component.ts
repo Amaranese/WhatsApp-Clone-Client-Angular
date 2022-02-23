49aa89b683df0ae40607f78b2a790e375a1578b0
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-02-23 10:40:48 +0100

    Create message-item.component.ts

---
+++ src/app/chat-viewer/components/message-item/message-item.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input} from '@angular/core';
+import {GetChat} from '../../../../graphql';
+
+@Component({
+  selector: 'app-message-item',
+  template: `
+    <div class="message"
+         [ngClass]="message.ownership ? 'message-mine' : 'message-other'">
+      <div *ngIf="isGroup && !message.ownership" class="message-sender">{{ message.sender.name }}</div>
+      <div class="message-content">{{ message.content }}</div>
+      <span class="message-timestamp">00:00</span>
+    </div>
+  `,
+  styleUrls: ['message-item.component.scss'],
+})
+export class MessageItemComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('item')
+  message: GetChat.Messages;
+
+  @Input()
+  isGroup: boolean;
+}
