5b9a2f7d5c712383a1c17ce76fc0340ed9c7e8b6
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-02-23 10:39:50 +0100

    Create chat-item.component.ts

---
+++ src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {GetChats} from '../../../../graphql';
+
+@Component({
+  selector: 'app-chat-item',
+  template: `
+    <div class="chat-row">
+      <img class="chat-pic" [src]="chat.picture || 'assets/default-profile-pic.jpg'">
+      <div class="chat-info">
+        <div class="chat-name">{{ chat.name }}</div>
+        <div class="chat-last-message">{{ chat.messages[chat.messages.length - 1]?.content }}</div>
+        <div class="chat-timestamp">00:00</div>
+      </div>
+    </div>
+  `,
+  styleUrls: ['chat-item.component.scss'],
+})
+export class ChatItemComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('item')
+  chat: GetChats.Chats;
+}
